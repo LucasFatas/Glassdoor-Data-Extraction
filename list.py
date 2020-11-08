72b8172d0f56be667d7b462775663734b339f661
Lucas Fatas <l.fataslynas@student.tudelft.nl>
2020-11-08 01:19:40 +0100

    Delete commented code

--- list.py
+++ list.py
@@ -6,8 +6,5 @@ import json
 
 with open('companyLists/giants.json') as json_file:
     data = json.load(json_file)
-    for x in range(23, 25):
+    for x in range(0, 25):
         Main.getData(data['companies'][x]['link'].rstrip(".htm") + "_P", data['companies'][x]['file'], 100)
-
-# for company in list:
-#    Main.getData(company[0].rstrip(".htm")+"_P", company[1], company[2])
